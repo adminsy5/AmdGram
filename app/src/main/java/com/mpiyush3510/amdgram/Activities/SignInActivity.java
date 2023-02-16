2a6320668ca57519657312a2ac34d0572470b929
PIYUSH MAKWANA <mpiyush3510@cbigujarat.onmicrosoft.com>
2023-02-16 15:30:16 +0530

    Ok

--- app/src/main/java/com/mpiyush3510/amdgram/Activities/SignInActivity.java
+++ app/src/main/java/com/mpiyush3510/amdgram/Activities/SignInActivity.java
@@ -3,7 +3,12 @@ package com.mpiyush3510.amdgram.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.mpiyush3510.amdgram.databinding.ActivitySignInBinding;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -20,5 +25,22 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners()
     {
         binding.createNewAccount.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        binding.btnSignIn.setOnClickListener(view -> addDataOnFireStore());
+    }
+
+    private void addDataOnFireStore()
+    {
+        FirebaseFirestore database=FirebaseFirestore.getInstance();
+        HashMap<String,Object>data=new HashMap<>();
+        data.put("First_Name","Piyush");
+        data.put("Last_Name","Makwana");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(this, "Data Inseted", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception -> {
+                    Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 }
