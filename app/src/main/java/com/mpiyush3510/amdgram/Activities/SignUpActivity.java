38e41be817b32b7408b591084dfdc0809ce8f993
PIYUSH MAKWANA <mpiyush3510@cbigujarat.onmicrosoft.com>
2023-02-16 17:02:08 +0530

    L3.5.18 Completed

--- app/src/main/java/com/mpiyush3510/amdgram/Activities/SignUpActivity.java
+++ app/src/main/java/com/mpiyush3510/amdgram/Activities/SignUpActivity.java
@@ -1,13 +1,28 @@
 package com.mpiyush3510.amdgram.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.mpiyush3510.amdgram.Utilities.Constants;
 import com.mpiyush3510.amdgram.databinding.ActivitySignUpBinding;
 
-import java.util.regex.Pattern;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -17,56 +32,113 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivitySignUpBinding.inflate(getLayoutInflater());
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
     }
-    private void setListeners()
-    {
+
+    private void setListeners() {
         binding.textSignIn.setOnClickListener(view -> onBackPressed());
         binding.btnSignUp.setOnClickListener(view -> {
-            if(isValidSignUpDetail())
-            {
+            if (isValidSignUpDetail()) {
                 signUp();
             }
         });
+        binding.layoutImage.setOnClickListener(view -> {
+            Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            PickImage.launch(intent);
+        });
     }
 
-    private void showToasts(String Message)
-    {
+    private void showToasts(String Message) {
         Toast.makeText(this, Message, Toast.LENGTH_SHORT).show();
     }
 
-    private void signUp()
-    {
+    private void signUp() {
+        isLoading(true);
+        FirebaseFirestore database =FirebaseFirestore.getInstance();
+        HashMap<String,Object> user=new HashMap<>();
+        user.put(Constants.KEY_NAME,binding.inputName.getText().toString());
+        user.put(Constants.KEY_EMAIL,binding.inputEmail.getText().toString());
+        user.put(Constants.KEY_PASSWORD,binding.inputPassword.getText().toString());
+        user.put(Constants.KEY_IMAGE,encodedImage);
+        database.collection(Constants.KET_COLLECTIONS_USERS)
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
+
+                })
+                .addOnFailureListener(exception ->{
+
+                });
+
 
     }
 
-    private Boolean isValidSignUpDetail()
+    private String encodedImage(Bitmap bitmap)
     {
-        if(encodedImage==null)
-        {
+        int previewWidth=150;
+        int previewHeight=bitmap.getHeight() * previewWidth / bitmap.getWidth();
+        Bitmap previewBitmap=Bitmap.createScaledBitmap(bitmap,previewWidth,previewHeight,false);
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG,50,byteArrayOutputStream);
+        byte[] bytes=byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes,Base64.DEFAULT);
+    }
+
+    private final ActivityResultLauncher<Intent> PickImage =registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode()==RESULT_OK){
+                    if(result.getData()!=null){
+                        Uri imageUri=result.getData().getData();
+                        try {
+                                InputStream inputStream=getContentResolver().openInputStream(imageUri);
+                                Bitmap bitmap= BitmapFactory.decodeStream(inputStream);
+                                binding.imageProfile.setImageBitmap(bitmap);
+                                binding.textAddImage.setVisibility(View.GONE);
+                                encodedImage =encodedImage(bitmap);
+                        }catch (FileNotFoundException ec) {
+                            ec.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
+    private Boolean isValidSignUpDetail() {
+        if (encodedImage == null) {
             showToasts("Select Profile Picture ");
             return false;
-        }else if(binding.inputName.getText().toString().isEmpty()) {
+        } else if (binding.inputName.getText().toString().isEmpty()) {
             showToasts("Please Enter Name ");
             return false;
-        } else if(binding.inputEmail.getText().toString().isEmpty()) {
+        } else if (binding.inputEmail.getText().toString().isEmpty()) {
             showToasts("Please Enter Email");
             return false;
         } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
             showToasts("Enter Valid Email");
             return false;
-        }else if (binding.inputPassword.getText().toString().isEmpty()){
+        } else if (binding.inputPassword.getText().toString().isEmpty()) {
             showToasts("Please Enter Password");
             return false;
         } else if (binding.inputConfirmPassword.getText().toString().isEmpty()) {
             showToasts("Please Enter Same Password ");
             return false;
+        } else {
+            return true;
+        }
+    }
+
+    private Boolean isLoading(Boolean isLoading) {
+        if (isLoading) {
+            binding.btnSignUp.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
         }
         else
         {
-            return true;
+            binding.btnSignUp.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.INVISIBLE);
         }
+         return isLoading;   //
     }
 }
---
+++ app/src/main/java/com/mpiyush3510/amdgram/Utilities/Constants.java
@@ -0,0 +1,12 @@
+package com.mpiyush3510.amdgram.Utilities;
+
+public class Constants {
+    public static final String KET_COLLECTIONS_USERS="users";
+    public static final String KEY_NAME="name";
+    public static final String  KEY_EMAIL="email";
+    public static final String KEY_PASSWORD="password";
+    public static final String KEY_PREFERENCE_NAME="AmdGramPreference";
+    public static final String KEY_IS_SIGNED_IN="isSignedIn";
+    public static final String KEY_USER_ID="userId";
+    public static final String KEY_IMAGE="image";
+}
